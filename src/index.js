696715a7cabdd2af0bd03699d2081f5543a40ddf
Artem Dybin <tema.dybin@yandex.ru>
2021-04-01 22:42:28 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,23 @@
+
+import './assets/js/scrollTo';
+import 'jquery';
+import mySwiper from './assets/js/swiper.js';
+import './assets/scss/styles.scss';
+import './assets/scss/normalize.scss';
+import './assets/img/FMC_logobook 1.png';
+import './assets/img/bck-head.png';
+import './assets/img/header.png';
+import './assets/img/advantages.png';
+import './assets/img/icons-head.png';
+import './assets/img/icons-comp.png';
+import './assets/img/icons-rocet.png';
+import './assets/img/icons-tooth.png';
+import './assets/img/5.png';
+import './assets/img/4.png';
+import './assets/img/3.png';
+import './assets/img/2.png';
+import './assets/img/1.png';
+import './assets/img/man.png';
+import './assets/img/present-back.png';
+
+mySwiper();
